dd915d7b21fd32b9a6bd4a251373d6ef50ff0f31
BryanStrk <bryan_ed_8@hotmail.com>
2026-04-27 09:07:31 +0200

    fix cors vercel

--- src/main/java/backend/javabean/config/SecurityConfig.java
+++ src/main/java/backend/javabean/config/SecurityConfig.java
@@ -62,10 +62,14 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:4200",
-                "https://frontend-angular-reto.vercel.app"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:4200",
+                "https://frontend-angular-reto.vercel.app",
+                "https://*.vercel.app"
+        ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
         config.setAllowCredentials(true);
 
